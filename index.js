9f1d78f9c38a645241776df90bdeb65c94b31504
Erica Droz <ericad2891@gmail.com>
2018-05-20 20:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name, holiday){
+  
+ return "Happy holidays, ${name}!"
+}
+return "Happy ${holiday}, ${name}!"
